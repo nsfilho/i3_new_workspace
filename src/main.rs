8d1b459cb414dba166559f1aecbe375c424d7b66
Nelio Santos <nsfilho@icloud.com>
2023-09-29 15:50:27 -0300

    feat(core): changing number ordering

--- src/main.rs
+++ src/main.rs
@@ -42,8 +42,26 @@ fn main() {
     let output = String::from_utf8(output.stdout).unwrap();
     let workspaces: Vec<Workspace> = serde_json::from_str(&output).unwrap();
 
-    // Pega o numero da proxima workspace
-    let next_workspace_number = workspaces.len() + 1;
+    // cria um novo array com o numero das workspaces ordenados, como numero inteiro
+    let mut workspaces: Vec<i32> = workspaces
+        .iter()
+        .filter_map(|workspace| workspace.name.parse::<i32>().ok())
+        .collect();
+
+    workspaces.sort_by(|a, b| a.cmp(b));
+
+    // Calcula o numero da nova workspace com base nas que ja existem
+    // exemplo 1: atualmente existem as workspaces 1, 3, 4, 5 -- então a nova workspace será a 2
+    // exemplo 2: atualmente existem as workspaces 1, 2, 3, 4 -- então a nova workspace será a 5
+    //
+    // Precisa correr o vetor de workspaces de modo ordenado para pegar o menor numero que não existe
+    let mut next_workspace_number = 1;
+    for workspace in workspaces {
+        if workspace != next_workspace_number {
+            break;
+        }
+        next_workspace_number += 1;
+    }
 
     // Cria a nova workspace
     Command::new("i3-msg")
